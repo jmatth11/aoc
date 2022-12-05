8e711e21d01e8212fa2c32027e3739f32f2fc588
Joshua Matthews <jmatth93@gmail.com>
2022-12-05 10:25:43 -0600

    added some comments

--- 02/rps.h
+++ 02/rps.h
@@ -1,10 +1,22 @@
 #ifndef AOC_RPS
 #define AOC_RPS
 
+/**
+ * Enums for Rock, Paper, Scissor.
+ */
 enum rps_t { ROCK, PAPER, SCISSORS, ERROR };
 
+/**
+ * Enums for result of a game.
+ */
 enum results_t { WIN, LOSE, DRAW };
 
+/**
+ * Evaluate the Rock, Paper, Scissors game.
+ * @param[in] const char The opponent's answer.
+ * @param[in] const char Your answer.
+ * @return int The resulting score.
+ */
 int evaluate_game(const char, const char);
 
 #endif
